3a7b27b8e086b21dee55754c4acb27d4856e27ca
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2023-05-22 23:17:40 +0300

    main

--- js/task7.js
+++
@@ -1,7 +0,0 @@
-const fontSizeControl = document.querySelector("#font-size-control");
-  const text = document.querySelector("#text");
-
-  fontSizeControl.addEventListener("input", () => {
-    const fontSize = fontSizeControl.value + "px";
-    text.style.fontSize = fontSize;
-  });
